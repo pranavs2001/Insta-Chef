d9d0042e65a03bd8f4b1ca7da8bb03b7773f9be9
Bradley Schulz <bradley@calahealth.com>
2021-02-21 19:16:49 -0800

    Implements modal window to search for ingredients to add to the pantry.
    API with themealdb is rewritten to be integrated with React and not a separate python script
    
    No styling is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import React from 'react'
+import Modal from 'react-modal'
 import './App.css';
 import Search from './search';
+import IngredientSearch from './ingredientsearch'
+
+Modal.setAppElement(document.getElementById('root'))
 
 function App() {
+  const [modalIsOpen,setIsOpen] = React.useState(false);
+
+  // Called when modal is opened
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // Stylistic changes after modal window opens
+  }
+
+  // called when modal is closed
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
     <div className="App">
       <p>Insta-chef</p>
-      <Search />
+      <button onClick={openModal}>Add new Ingredient</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        contentLabel="Ingredient Search Box"
+      >
+        <button onClick={closeModal}>Done</button>
+        <IngredientSearch/>
+      </Modal>
+      <div>
+        <Search />
+      </div>
     </div>
   );
 }
