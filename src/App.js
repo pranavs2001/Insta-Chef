05d9466fc7fbf89a2f659a6389bd4159f09a7392
Solaine Zhao <solainezh2019@gmail.com>
2021-02-26 00:07:41 -0800

    add navbar with routing

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import './App.css';
 import LoginPage from './Components/SignIn/LoginPage';
 import Search from './search';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-// import NavBar from './Components/NavigationBar/NavBar';
-//import Navbar from './Components/Navbar';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <LoginPage/>
-      {/* <Navbar/> */}
-      {/* <Switch> */}
-        {/* <Route path="/" exact component={Home}/>
+      <Navbar/>
+      <Switch>
+        <Route path="/" exact component={Home}/>
         <Route path="/Login" component={LoginPage}/>
-        <Route path="/Search" component={Search}/> */}
-      {/* <p>Insta-chef</p> */}
-      {/* <LoginPage/> */}
-      {/* </Switch> */}
+        <Route path="/Search" component={Search}/>
+      <p>Insta-chef</p>
+      <LoginPage/>
+      </Switch>
     </div>
     </Router>
     
