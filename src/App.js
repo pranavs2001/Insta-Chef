fe448baa46fabe5905b920d56bc5e7807655932f
Solaine Zhao <solainezh2019@gmail.com>
2021-02-25 23:48:33 -0800

    add user authentication and login/signin UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
+import LoginPage from './Components/SignIn/LoginPage';
 import Search from './search';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+// import NavBar from './Components/NavigationBar/NavBar';
+//import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <p>Insta-chef</p>
-      <Search />
+    <Router>
+      <div className="App">
+        <LoginPage/>
+      {/* <Navbar/> */}
+      {/* <Switch> */}
+        {/* <Route path="/" exact component={Home}/>
+        <Route path="/Login" component={LoginPage}/>
+        <Route path="/Search" component={Search}/> */}
+      {/* <p>Insta-chef</p> */}
+      {/* <LoginPage/> */}
+      {/* </Switch> */}
     </div>
+    </Router>
+    
   );
 }
 
+const Home = () => (
+  <div>
+    <h1> Home Page</h1>
+  </div>
+)
 export default App;
---
+++ src/Components/SignIn/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Hero = ({handleLogout}) => {
+
+    return(
+        <section className="Hero">
+            <nav>
+                <h2> Welcome</h2>
+                <button onClick={handleLogout}> Logout </button>
+            </nav>
+        </section>
+    )
+}
+
+export default Hero;
