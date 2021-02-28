fa54b1201f418ea489c96d831b7bcb25a3b571f7
Jonathan Carlson <jonathanbcarlson@gmail.com>
2021-02-27 20:33:54 -0800

    adds ability for user to view their pantry items stored in firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LoginPage from './Components/SignIn/LoginPage';
 import Search from './search';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './Components/Navbar';
+import Pantry from './Components/Pantry/Pantry';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" exact component={Home}/>
         <Route path="/Login" component={LoginPage}/>
         <Route path="/Search" component={Search}/>
+        <Route path="/Pantry" component={Pantry}/>
       <p>Insta-chef</p>
       <LoginPage/>
       </Switch>
--- src/Components/Pantry/Pantry.js
+++ src/Components/Pantry/Pantry.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import firebase from "../firebase"
+import fire from "../SignIn/fire"
 import "firebase/database"
 
 class Pantry extends React.Component {
@@ -9,17 +9,45 @@ class Pantry extends React.Component {
             items: {},
             uid: '',
             itemAdded: '',
+            loggedIn: false,
         };
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.viewPantry = this.viewPantry.bind(this);
+        this.addItemForm = this.addItemForm.bind(this);
     }
 
-    /*
-    user has a form where they can add items to their pantry
-    user can see items in their pantry
-    pantry items are added to firestore, and connected to uid
-    */
+   componentDidMount() {
+       //get latest pantry items 
+       //if user is logged in
+       if (fire.auth().currentUser) {
+           let uid = fire.auth().currentUser.uid;
+           var itemsRef = fire.database().ref(uid + 'pantryItems/').orderByChild('items');
+           itemsRef.on('value', (snapshot) => {
+               let items = [];
+               snapshot.forEach((childSnapshot) => {
+                   let val  = childSnapshot.val();
+                   items.push(val);
+               })
+                this.setState({
+                    items: items,
+                    uid: uid,
+                    loggedIn: true,
+                });
+            })
+        }
+   }
 
+    viewPantry() {
+        if(this.state.loggedIn) {
+            let items = this.state.items;
+            return items.map((elem, index) => {
+                return <li key={index}>{elem.item}</li>
+            })
+        } else {
+            return null;
+        }
+    }
 
     handleChange(e) {
         this.setState({ itemAdded: e.target.value });
@@ -28,26 +56,49 @@ class Pantry extends React.Component {
     // add item to pantry on submit
     handleSubmit(e) {
         e.preventDefault();
-        console.log('An item was submitted: ' + this.state.itemAdded);
-        const itemsRef = firebase.database().ref('pantryItems');
-        const item = {
-            item: this.state.itemAdded,
+        if(this.state.loggedIn)
+        {
+            console.log('An item was submitted: ' + this.state.itemAdded);
+            var itemRef = fire.database().ref(this.state.uid + 'pantryItems/');
+            var newItemRef = itemRef.push();
+            newItemRef.set({
+                item: this.state.itemAdded,
+            })
+            this.setState({
+                itemAdded: '',
+            })
+        } 
+    }
+
+    addItemForm() {
+        if(this.state.loggedIn) {
+            return(
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Add an item to your Pantry:
+                            <input type="text" value={this.state.itemAdded} onChange={this.handleChange} />
+                    </label>
+                    <input type="submit" value="Add" />
+                </form>
+            )
+        } else {
+            return(
+                <h3>You need to login before viewing your Pantry.</h3>
+            )
         }
-        itemsRef.push(item);
-        this.setState({
-            itemAdded: '',
-        })
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Add an item to your Pantry:  
-                    <input type="text" value={this.state.itemAdded} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Add" />
-            </form>
+            <div className="test">
+                <this.addItemForm/>
+                <div className="pantry" style={{display: "flex", justifyContent: "center"}}>
+                    {/* <button onClick={this.viewPantry}> view pantry </button> */}
+                    <div className="pantryItems" style={{diplay: "inlineBlock", textAlign: "left"}}>
+                        <this.viewPantry/>
+                    </div>
+                </div>
+            </div>
         );
     }
 
