73ca588cfb2cc76f455d2372275aaec4b6f46fdf
Jonathan Carlson <jonathanbcarlson@gmail.com>
2021-03-07 10:43:09 -0800

    fixes bug where recipes for different ingredients in the SearchInPantryModal; also converted PantryGrid to a class

--- src/Components/Pantry/PantryGrid.js
+++ src/Components/Pantry/PantryGrid.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core';
 import { faSearch, faWindowClose } from "@fortawesome/free-solid-svg-icons";
@@ -29,56 +29,80 @@ const useStyles = makeStyles((theme) => ({
 
 let recipeIDs = [];
 
-export default function PantryGrid(props) {
-  const classes = useStyles();
-  const [isOpen, setOpen] = useState(false);
-  const afterOpen = () => {};
-  const closeModal = () => {setOpen(false)};
-  const openModal = () => {setOpen(true)};
-  const [isLoaded, setLoaded] = useState(false);
-  return (
-    <div>
-      <div style={{ marginTop: "30px" }} >
-        <Grid container spacing={1}>
-          {Object.keys(props.ingredients).map((key, id) => (
-            <Grid key={key} item xs={3}>
-              <div>
-                <button className={classes.button} onClick={() => {setLoaded(getRecipeIDs(recipeIDs, props.uid, key, openModal))}}>
-                  Search {<FontAwesomeIcon icon={faSearch} />}
-                </button>
-                <SearchInPantryModal
-                  isLoaded={isLoaded}
-                  isOpen={isOpen}
-                  afterOpen={afterOpen}
-                  closeModal={closeModal}
-                  ingredKey={key}
-                  uid={props.uid}
-                  recipeIDs={recipeIDs}
-                />
-                <button className={classes.button} onClick={() => props.removeItemFromPantry(key, props.loggedIn, props.uid)}>
-                  Remove {<FontAwesomeIcon icon={faWindowClose} />}
-                </button>
-                <li style={{marginBottom: "10px"}} key={key}>{props.ingredients[key].item}</li>
-              </div>
-            </Grid>
-          ))}
-        </Grid>
+class PantryGrid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+      isLoaded: false,
+      recipeIDs: [],
+      ingredients: props.ingredients,
+      loggedIn: props.loggedIn,
+      uid: props.uid,
+      removeItemFromPantry: props.removeItemFromPantry
+    }
+  }
+
+  componentDidMount() {
+    // const classes = useStyles();
+  }
+  
+  afterOpen = () => {};
+  closeModal = () => {this.setState({isOpen: false})};
+  openModal = () => { this.setState({ isOpen: true }) };
+
+  getRecipeIDs = (key) => {
+      let recipeIDs = [];
+      let pantryRef = fire.database().ref(this.state.uid + '/pantryItems/' + key.toString());
+      let recipesForIngred = pantryRef.child('recipeIDs')
+      recipesForIngred.on('value', (snapshot) => {
+        // loop through ingredient's associated recipeIDs
+        snapshot.forEach((childSnapshot) => {
+          recipeIDs.push(childSnapshot.val())
+        })
+      })
+      console.log('recipeIds in getrecipieIds are: ', recipeIDs);
+      this.setState({
+        loaded: true,
+        isOpen: true,
+        recipeIDs: recipeIDs,
+      })
+  };
+
+  render () {
+    return(
+      <div>
+        <div style={{ marginTop: "30px" }} >
+          <Grid container spacing={1}>
+            {Object.keys(this.state.ingredients).map((key, id) => (
+              <Grid key={key} item xs={3}>
+                <div>
+                  {/*className={classes.button}*/}
+                  <button onClick={() => this.getRecipeIDs(key)}> 
+                    Search {<FontAwesomeIcon icon={faSearch} />}
+                  </button>
+                  <SearchInPantryModal
+                    isLoaded={this.state.isLoaded}
+                    isOpen={this.state.isOpen}
+                    afterOpen={this.afterOpen}
+                    closeModal={this.closeModal}
+                    ingredKey={key}
+                    uid={this.state.uid}
+                    recipeIDs={this.state.recipeIDs}
+                  />
+                  {/* className={classes.button} */}
+                  <button onClick={() => this.state.removeItemFromPantry(key, this.state.loggedIn, this.state.uid)}>
+                    Remove {<FontAwesomeIcon icon={faWindowClose} />}
+                  </button>
+                  <li style={{marginBottom: "10px"}} key={key}>{this.state.ingredients[key].item}</li>
+                </div>
+              </Grid>
+            ))}
+          </Grid>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-const getRecipeIDs = (recipeIDs, uid, key, openModal) => {
-  let pantryRef = fire.database().ref(uid + '/pantryItems/' + key.toString());
-  let recipesForIngred = pantryRef.child('recipeIDs')
-  recipesForIngred.on('value', (snapshot) => {
-    // loop through ingredient's associated recipeIDs
-    snapshot.forEach((childSnapshot) => {
-      recipeIDs.push(childSnapshot.val())
-    })
-  })
-  console.log('recipeIds in getrecipieIds are: ', recipeIDs);
-  openModal()
-  return true;
-}
-
+export default PantryGrid;
