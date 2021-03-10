b567e5044c88203ad2830b3750ed83f3438b887f
Drake Cote <79180618+drakecote@users.noreply.github.com>
2021-03-10 00:10:26 -0800

    Drakes/css bradleys stuff (#42)
    * Add favorite recipes tab and being reformatting elements
    
    * fills background color to entire page
    
    * adds favoriteRecipies
    
    * adds NameForm to clean up render in Profile
    
    * adds ChangePasswordForm and DeleteAccountForm to clean up render
    
    * adds CurrentPasswordForm to clean up render
    
    * add profile.css
    
    * adds forms for all input types
    
    * adds icon for search and moves styling to search.css
    
    * css home eggert contact
    
    * update profile and main page css/formatting
    
    Co-authored-by: bschulz01 <bradley@calahealth.com>
    Co-authored-by: Jonathan Carlson <jonathanbcarlson@gmail.com>

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,53 +1,260 @@
 import React from 'react'
 import fire from "../SignIn/fire";
-import RecipeGrid from "../Tile/RecipeGrid";
-import UserInfo from "./UserInfo";
+import Modal from 'react-modal';
+import './profile.css'
+import { faUser, faSearch, faWindowClose, faKey } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Form } from 'react-bootstrap'
 
 class Profile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      favoriteRecipes: [],
-      uid: '',
+      newName: '',
+      newPassword1: '',
+      newPassword2: '',
+      enteredPassword: '',
+      actionMsg: '',
+      requestDelete: false,
     };
-    this.getFavorites = this.getFavorites.bind(this);
+
+    this.updateUserName = this.updateUserName.bind(this);
+    this.updatePassword = this.updatePassword.bind(this);
+    this.toggleDeleteModal = this.toggleDeleteModal.bind(this);
+    this.deleteAccount = this.deleteAccount.bind(this);
+    this.currentInfo = this.currentInfo.bind(this);
+    this.ChangePasswordForm = this.ChangePasswordForm.bind(this);
+    this.DeleteAccountForm = this.DeleteAccountForm.bind(this);
+    this.CurrentPasswordForm = this.CurrentPasswordForm.bind(this);
   }
 
-  componentDidMount() {
-    this.getFavorites()
-  }
-
-  getFavorites() {
-    if (fire.auth().currentUser) {
-      const uid = fire.auth().currentUser.uid;
-      // Automatically create "other" category if it doesn't exist
-      let pantryRef = fire.database().ref(uid + '/favorites').orderByChild('id');
-      pantryRef.on('value', (snapshot) => {
-        let recipeIDs = [];
-        snapshot.forEach((childSnapshot) => {
-          recipeIDs.push({
-            'id': childSnapshot.val()['id'],
-            'name': childSnapshot.val()['name'],
-          });
-        });
+  updateUserName() {
+    const user = fire.auth().currentUser;
+    if (this.state.newName !== '') {
+      user.updateProfile({
+        displayName: this.state.newName,
+      }).then(() => {
         this.setState({
-          favoriteRecipes: recipeIDs,
-          uid: uid,
-        })
-      });
+          actionMsg: "Username Updated",
+          newName: '',
+        });
+      }).catch(() => {
+        console.log("Error updating profile")
+      })
+    } else {
+      this.setState({
+        actionMsg: "Enter a username"
+      })
     }
   }
 
-  render() {
-    return (
+  updatePassword() {
+    // Non-matching passwords
+    if (this.state.newPassword1 !== this.state.newPassword2) {
+      this.setState({
+        actionMsg: "ERROR: Passwords must match",
+      });
+    } else {
+      // Invalid password
+      if (this.state.newPassword1.length < 6) {
+        this.setState({
+          actionMsg: "Invalid password"
+        });
+        return;
+      }
+      // Update password
+      console.log("Updating Password, entered password = ", this.state.enteredPassword);
+      const user =fire.auth().currentUser;
+      fire.auth().signInWithEmailAndPassword(user.email, this.state.enteredPassword).then(() => {
+        fire.auth().currentUser.updatePassword(this.state.newPassword1).then(() => {
+          this.setState({
+            newPassword1: '',
+            newPassword2: '',
+            actionMsg: "Password Updated",
+          });
+        }).catch(() => {
+          this.setState({
+            actionMsg: "Error updating password. Try signing in again"
+          });
+        })
+      }).catch(() => {
+        this.setState({
+          actionMsg: "Entered current password is invalid",
+        })
+      })
+    }
+  }
+
+  toggleDeleteModal() {
+    this.setState({
+      requestDelete: !this.state.requestDelete,
+      enteredPassword: '',
+    })
+  }
+
+  deleteAccount() {
+    const user = fire.auth().currentUser;
+    console.log("Deleting account");
+    fire.auth().signInWithEmailAndPassword(user.email, this.state.enteredPassword).then(() => {
+      user.delete().catch(() => {
+        this.setState({
+          requestDelete: false,
+          actionMsg: "Error deleting account",
+        })
+      })
+    }).catch(() => {
+      this.setState({
+          requestDelete: false,
+          actionMsg: "Error deleting account",
+        })
+    })
+
+  }
+
+  currentInfo(props) {
+    return(
       <div>
-        <h1>Favorite Recipes</h1>
-        <hr/>
-        <RecipeGrid recipes={this.state.favoriteRecipes} callback={this.getFavorites}/>
-        <UserInfo/>
+      <div /*className = "current_Info"*/>
+        <h3>Current Info:</h3>
+        <p1> Name: {props.user.displayName}</p1>
+        <div>
+            Email: {props.user.email}
+        </div>
+      </div>
+          <h3> Update name: </h3>
+          <body>
+            <div className="input-container">
+              <icon className= "icon"> {<FontAwesomeIcon icon={faUser} />} </icon>
+                <input
+                  className="input-field"
+                  type="text"
+                  name="name"
+                  value={this.state.newName}
+                  placeholder={"Enter name"}
+                  onChange={(e) => { this.setState({ newName: e.target.value }); }}
+                />
+            </div>
+          </body>
+          <button onClick={this.updateUserName}>Change Username</button>
+      </div>
+    )
+  }
+
+
+  ChangePasswordForm(props) {
+    return(
+      <div>
+        <form>
+          <div className="input-container">
+            <icon className= "icon"> {<FontAwesomeIcon icon={faKey} />} </icon>
+            <input
+              className = "input-field"
+              value={this.state.newPassword1}
+              type="password"
+              placeholder={"New Password"}
+              onChange={(e) => {
+                this.setState({
+                  updatedProfile: false,
+                  newPassword1: e.target.value,
+                });
+              }}
+              autoComplete="new-password"
+            />
+          </div>
+        </form>
+        <form>
+          <div className="input-container">
+            <icon className= "icon"> {<FontAwesomeIcon icon={faKey} />} </icon>
+            <input
+              className = "input-field"
+              value={this.state.newPassword2}
+              type="password"
+              placeholder={"Confirm Password"}
+              onChange={(e) => { this.setState({ newPassword2: e.target.value }); }}
+              autoComplete="new-password"
+            />
+          </div>
+        </form>
+        <button onClick={this.updatePassword}>Change Password</button>
+      </div>
+    )
+  }
+
+  CurrentPasswordForm() {
+    return(
+    <div>
+      <form>
+       <div className="input-container">
+            <icon className= "icon"> {<FontAwesomeIcon icon={faKey} />} </icon>
+          <input
+            className = "input-field"
+            value={this.state.enteredPassword}
+            type="password"
+            placeholder={"Current Password"}
+            onChange={(e) => { this.setState({ enteredPassword: e.target.value }); }}
+            autoComplete="current-password"
+          />
+        </div>
+      </form>
+    </div>
+    )
+  }
+
+  DeleteAccountForm() {
+    return(
+      <div>
+        <h3>Delete Acount:</h3>
+        <button onClick={this.toggleDeleteModal}>DELETE ACCOUNT</button>
+        <Modal
+              isOpen={this.state.requestDelete}
+              // onAfterOpen={afterOpenModal}
+              // onRequestClose={closeModal}
+              contentLabel="Request delete account"
+          >
+          <h1>ARE YOU SURE YOU WANT TO DELETE THIS ACCOUNT?</h1>
+          <button onClick={this.toggleDeleteModal}>Cancel</button>
+          <h1>Enter password to confirm:</h1>
+          <form>
+            <div className="input-container">
+              <icon className= "icon"> {<FontAwesomeIcon icon={faKey} />} </icon>
+              <input
+                className = "input-field"
+                value={this.state.enteredPassword}
+                type="password"
+                placeholder={"Enter Password"}
+                onChange={(e) => {this.setState({enteredPassword: e.target.value});}}
+                autoComplete="current-password"
+              />
+            </div>
+          </form>
+          <button onClick={this.deleteAccount}>
+            Confirm Delete
+          </button>
+        </Modal>
+      </div>
+    )
+  }
+
+  render() {
+    const user = fire.auth().currentUser;
+    return (
+      <div className="main-profile-box">
+        <div>
+          <h1>Account Info</h1>
+          <hr/>
+          <p style={{color: 'red'}}>
+            {this.state.actionMsg !== '' ? this.state.actionMsg : null}
+          </p>
+          <this.currentInfo user={user}/>
+          <h3>Change Password:</h3>
+          <this.CurrentPasswordForm/>
+          <this.ChangePasswordForm/>
+          <this.DeleteAccountForm/>
+        </div>
       </div>
     );
   }
 }
+
 
 export default Profile;
