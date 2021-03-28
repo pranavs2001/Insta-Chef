5cee03919f391bfb5290ba6e62d92fef00a7dae8
Jonathan Carlson <jonathanbcarlson@gmail.com>
2021-03-27 20:44:02 -0700

    adds pranav, solaine, bradley, drake profiles

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,11 @@
-import './Home.css'
-import eggertProfilePhoto from '../../img/eggert.png'
-import jonathanProfilePhoto from '../../img/team/jonathan.jpg'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faSearch } from "@fortawesome/free-solid-svg-icons";
-import { orange, red } from '@material-ui/core/colors';
+import './Home.css';
+import TeamProfile from './TeamProfile'
+
+import eggertProfilePhoto from '../../img/eggert.png';
+import jonathanProfilePhoto from '../../img/team/jonathanProfilePhoto.jpg';
+import pranavProfilePhoto from '../../img/team/pranavProfilePhoto.jpg';
+import solaineProfilePhoto from '../../img/team/solaineProfilePhoto.jpg';
+import bradleyProfilePhoto from '../../img/team/bradleyProfilePhoto.jpg';
 
 const Home = () => (
   <div>
@@ -19,140 +21,66 @@ const Home = () => (
       <p> Pranav Srinivasan: loves to dunk on Bradley<br /></p> */}
   
       <div className="member-grid">
-        <div className="grid-row" id="jonathan">
-          <div className="grid-col">
-            <div className="profile-img-container">
-              <img className="profile-img" src={jonathanProfilePhoto} alt="picture of Jonathan Carlson " />
-            </div>
-          </div>
-          <div className="grid-col">
-            <div className="member-content">
-              <h3> Jonathan Carlson
-                <span className="pronouns"> (he/him) </span>
-              </h3>
-              <p className="member-position">
-                Developer
-              </p>
-              <a target="_blank" rel="noopener noreferrer" class="profile-link" href="mailto:jonathanbcarlson@gmail.com">
-                {<FontAwesomeIcon icon={faEnvelope} style={{ paddingRight: "5px" }} size="lg" />}
-                jonathanbcarlson@gmail.com
-              </a>
-              <p/>
-              <a target="_blank" rel="noopener noreferrer" class="profile-link"  href="https://github.com/jonathanbcarlson">
-                <i class="fab fa-github fa-1x" style={{ paddingRight: "5px" }} ></i>
-                jonathanbcarlson
-              </a>
-            </div>
-          </div>
-        </div>
-        
-        <div className="grid-row" id="pranav">
-          <div className="grid-col">
-            <div className="profile-img-container">
-              <img className="profile-img" src={jonathanProfilePhoto} alt="picture of Jonathan Carlson " />
-            </div>
-          </div>
-          <div className="grid-col">
-            <div className="member-content">
-              <h3> Jonathan Carlson
-                <span className="pronouns"> (he/him) </span>
-              </h3>
-              <p className="member-position">
-                Developer
-              </p>
-              <a target="_blank" rel="noopener noreferrer" class="profile-link" href="mailto:jonathanbcarlson@gmail.com">
-                {<FontAwesomeIcon icon={faEnvelope} style={{ paddingRight: "5px" }} size="lg" />}
-                jonathanbcarlson@gmail.com
-              </a>
-              <p />
-              <a target="_blank" rel="noopener noreferrer" class="profile-link" href="https://github.com/jonathanbcarlson">
-                <i class="fab fa-github fa-1x" style={{ paddingRight: "5px" }} ></i>
-                jonathanbcarlson
-              </a>
-            </div>
-          </div>
-        </div>
-        
-        <div className="grid-row" id="solaine">
-          <div className="grid-col">
-            <div className="profile-img-container">
-              <img className="profile-img" src={jonathanProfilePhoto} alt="picture of Jonathan Carlson " />
-            </div>
-          </div>
-          <div className="grid-col">
-            <div className="member-content">
-              <h3> Jonathan Carlson
-                <span className="pronouns"> (he/him) </span>
-              </h3>
-              <p className="member-position">
-                Developer
-              </p>
-              <a target="_blank" rel="noopener noreferrer" class="profile-link" href="mailto:jonathanbcarlson@gmail.com">
-                {<FontAwesomeIcon icon={faEnvelope} style={{ paddingRight: "5px" }} size="lg" />}
-                jonathanbcarlson@gmail.com
-              </a>
-              <p />
-              <a target="_blank" rel="noopener noreferrer" class="profile-link" href="https://github.com/jonathanbcarlson">
-                <i class="fab fa-github fa-1x" style={{ paddingRight: "5px" }} ></i>
-                jonathanbcarlson
-              </a>
-            </div>
-          </div>
-        </div>
-        
-        <div className="grid-row" id="bradley">
-          <div className="grid-col">
-            <div className="profile-img-container">
-              <img className="profile-img" src={jonathanProfilePhoto} alt="picture of Jonathan Carlson " />
-            </div>
-          </div>
-          <div className="grid-col">
-            <div className="member-content">
-              <h3> Jonathan Carlson
-                <span className="pronouns"> (he/him) </span>
-              </h3>
-              <p className="member-position">
-                Developer
-              </p>
-              <a target="_blank" rel="noopener noreferrer" class="profile-link" href="mailto:jonathanbcarlson@gmail.com">
-                {<FontAwesomeIcon icon={faEnvelope} style={{ paddingRight: "5px" }} size="lg" />}
-                jonathanbcarlson@gmail.com
-              </a>
-              <p />
-              <a target="_blank" rel="noopener noreferrer" class="profile-link" href="https://github.com/jonathanbcarlson">
-                <i class="fab fa-github fa-1x" style={{ paddingRight: "5px" }} ></i>
-                jonathanbcarlson
-              </a>
-            </div>
-          </div>
-        </div>
+        <TeamProfile 
+          id="jonathan" 
+          profileImgSrc={jonathanProfilePhoto}
+          imgAlt="picture of Jonathan Carlson" 
+          name="Jonathan Carlson"
+          pronouns="he/him"
+          memberPosition="Developer"
+          email="jonathanbcarlson@gmail.com"
+          githubUsername="jonathanbcarlson"
+          linkedinUsername="jonathanbcarlson"
+        />
 
-        <div className="grid-row" id="drake">
-          <div className="grid-col">
-            <div className="profile-img-container">
-              <img className="profile-img" src={jonathanProfilePhoto} alt="picture of Jonathan Carlson " />
-            </div>
-          </div>
-          <div className="grid-col">
-            <div className="member-content">
-              <h3> Jonathan Carlson
-                <span className="pronouns"> (he/him) </span>
-              </h3>
-              <p className="member-position">
-                Developer
-              </p>
-              <a target="_blank" rel="noopener noreferrer" class="profile-link" href="mailto:jonathanbcarlson@gmail.com">
-                {<FontAwesomeIcon icon={faEnvelope} style={{ paddingRight: "5px" }} size="lg" />}
-                jonathanbcarlson@gmail.com
-              </a>
-              <p />
-              <a target="_blank" rel="noopener noreferrer" class="profile-link" href="https://github.com/jonathanbcarlson">
-                <i class="fab fa-github fa-1x" style={{ paddingRight: "5px" }} ></i>
-                jonathanbcarlson
-              </a>
-            </div>
-          </div>
-        </div>
+        <TeamProfile 
+          id="pranav"
+          profileImgSrc={pranavProfilePhoto}
+          imgAlt="picture of Pranav Srinivasan"
+          name="Pranav Srinivasan"
+          pronouns=""
+          memberPosition="Developer"
+          email="pranavsankars@g.ucla.edu"
+          githubUsername="pranavs2001"
+          linkedinUsername="pranavsankars"
+        />
+
+        <TeamProfile
+          id="solaine"
+          profileImgSrc={solaineProfilePhoto}
+          imgAlt="picture of Solaine Zhao"
+          name="Solaine Zhao"
+          pronouns=""
+          memberPosition="Developer"
+          email="solainezh2019@gmail.com"
+          githubUsername="solainezhao"
+          linkedinUsername="solaine-zhao-5aa04a1b1"
+        />
+          
+        <TeamProfile 
+          id="bradley"
+          profileImgSrc={bradleyProfilePhoto}
+          imgAlt="picture of Bradley Schulz"
+          name="Bradley Schulz"
+          pronouns=""
+          memberPosition="Developer"
+          email="bschulz267@ucla.edu"
+          githubUsername="bschulz01"
+          linkedinUsername="bradley-schulz"
+        />
+
+        <TeamProfile
+          id="drake"
+          profileImgSrc={""}
+          imgAlt="picture of Drake Cote"
+          name="Drake Cote"
+          pronouns=""
+          memberPosition="Developer"
+          email="drakecote@g.ucla.edu"
+          githubUsername="drakecote"
+          linkedinUsername=""
+        />
+
       </div>
 
       <div class="management">
